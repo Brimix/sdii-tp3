6decefcfb92367e8a71fcb09cb7e83e9cf0d3b59
Brian Morris <31920289+Brimix@users.noreply.github.com>
2023-06-10 12:23:58 -0300

    Refactor clean code (#11)
    * Format documentation, add spaces and encapsulate interactor
    
    * Fix duplicate definition
    
    * fix hardfault
    
    ---------
    
    Co-authored-by: tomasfgonzalez <103332587+tomasfgonzalez@users.noreply.github.com>

--- source/efHal/uart_ringBuffer.h
+++ source/efHal/uart_ringBuffer.h
@@ -4,23 +4,26 @@
 
 /*==================[inclusions]=============================================*/
 
-// Standard C Included Files
+/* Standard C Included Files */
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "stdint.h"
-// Project Included Files
+#include <stdint.h>
+
+/* Project Included Files */
 #include "bsp_kl46/SD2_board.h"
-#include "fsl_uart.h"
 #include "MKL46Z4.h"
 #include "ringBuffer.h"
+#include "fsl_uart.h"
 #include "fsl_uart_dma.h"
 #include "fsl_dmamux.h"
+
 /*==================[cplusplus]==============================================*/
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/*==================[external functions definition]==========================*/
 void uart_ringBuffer_init(void);
 int32_t uart_ringBuffer_recDatos(uint8_t *pBuf, int32_t size);
 int32_t uart_dma_envDatos(uint8_t *pBuf, int32_t size);
--- source/fsm/frameProcessor/frameProcessor.c
+++ source/fsm/frameProcessor/frameProcessor.c
@@ -1,16 +1,42 @@
-/*
- * IMPORTANT!
+/* Copyright 2017, DSI FCEIA UNR - Sistemas Digitales 2
+ *    DSI: http://www.dsi.fceia.unr.edu.ar/
+ * Copyright 2023, Brian Morris, Tomas Gonzalez
+ * All rights reserved.
  *
- * This module should be renamed and moved out of this folder.
- * And recoded, please...
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
  */
-#include "interactor.h"
 
+#include "frameProcessor.h"
+
+/*==================[internal data definition]==========================*/
 uint8_t bufferEnv[32];
 FrameData_t receivedFrame;
 bool responseReady = false;
-
-uint8_t* get_bufferEnv() { return bufferEnv; }
 
 void LEDS() { //cuando se detecta accion sobre LED
 	switch (receivedFrame.actionCode[0]) {
@@ -93,6 +119,9 @@ void ACELEROM() { //cuando se detecta medicion de la aceleracion
 	}
 }
 
+/*==================[external functions definition]==========================*/
+uint8_t* get_bufferEnv() { return bufferEnv; }
+
 bool processFrame(char *bufferRec, int length) { //ejecuta función segun periferico que deba intervenir
 	strcpy(receivedFrame.data, bufferRec);
 	receivedFrame.data[length] = '\0';
@@ -112,3 +141,5 @@ bool processFrame(char *bufferRec, int length) { //ejecuta función segun perife
 	}
 	return responseReady;
 }
+
+/*==================[end of file]============================================*/
